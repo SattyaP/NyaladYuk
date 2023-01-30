fc488a0c4bc4e1f8411c5eeafaaf9cf93167effa
SiMamank115 <faizfullafalisa@gmail.com>
2023-01-30 17:38:27 +0700

    anim minor

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-document.onreadystatechange = () => {
-  document.querySelector(".loading").remove()
-}
-
 const items = document.querySelectorAll("nav ul li");
 items.forEach((item) => {
-  item.addEventListener("click", () => {
-    document.querySelector("li.active").classList.remove("active");
-    item.classList.add("active");
-  });
+    item.addEventListener("click", () => {
+        document.querySelector("li.active").classList.remove("active");
+        item.classList.add("active");
+    });
 });
-
-
-
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 $(document).ready(function () {
-  $('.banner').slick({
-    infinite: true,
-    speed: 300,
-    autoplay: true,
-  });
+    $(".banner").slick({
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+    });
+	setTimeout(() => {
+		$(".loading").remove();
+	}, 100);
+    return;
 });
-
